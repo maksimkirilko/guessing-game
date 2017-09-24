c7f4232ddb7cd8a25b71bf92173f200ac63b54e1
Maksim Kirilko <maksim.kirilko@intellectsoft.net>
2017-09-24 12:34:48 +0300

    Implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
+
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.result = Math.round(this.min + (this.max - this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
